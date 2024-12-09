3cff3914a647bb272351d2927593532eb30b1c8b
joetaylorland <accounts+github@joetaylor.net>
2024-12-09 08:52:56 -0800

    demo delight 4

--- packages/ui/src/views/repo/repo-summary/repo-summary.tsx
+++ packages/ui/src/views/repo/repo-summary/repo-summary.tsx
@@ -3,6 +3,7 @@
 import { useNavigate } from 'react-router-dom'
 
 import {
+  Badge,
   Button,
   ButtonGroup,
   DropdownMenu,
@@ -99,7 +100,7 @@ export function RepoSummaryView({
 
   return (
     <SandboxLayout.Main hasLeftPanel hasHeader hasSubHeader fullWidth>
-      <SandboxLayout.Columns columnWidths="1fr 300px">
+      <SandboxLayout.Columns columnWidths="1fr 220px">
         <SandboxLayout.Column>
           <SandboxLayout.Content>
             <ListActions.Root>
@@ -177,8 +178,8 @@ export function RepoSummaryView({
           </SandboxLayout.Content>
         </SandboxLayout.Column>
         <SandboxLayout.Column>
-          <SandboxLayout.Content className="pl-0">
-            <SummaryPanel
+          <SandboxLayout.Content className="sticky top-[100px] pl-0 flex flex-col gap-7">
+            {/* <SummaryPanel
               title={t('views:repos.summary', 'Summary')}
               details={[
                 {
@@ -211,7 +212,144 @@ export function RepoSummaryView({
               isEditingDescription={isEditingDescription}
               setIsEditingDescription={setIsEditingDescription}
               saveDescription={saveDescription}
-            />
+            /> */}
+            <div className="flex flex-col gap-3">
+              <div className="flex gap-2.5 justify-between items-center">
+                <Text size={3} className="tracking-snug text-foreground-1 col-span-2">
+                  {t('views:repos.summary', 'Summary')}
+                </Text>
+                <Button variant="ghost" size="sm_icon" aria-label="More options">
+                  <Icon name="more-dots-fill" size={12} className="text-icons-3" />
+                </Button>
+              </div>
+              <div className="relative flex w-full py-3 px-2.5 bg-background-4 flex-col gap-y-3 gap-x-2.5 text-center border rounded-md items-start">
+                <div className="grid grid-cols-2 gap-2">
+                  <div className="flex flex-col justify-center rounded-md bg-background-4/90 py-1 px-3">
+                    <Text size={3} className="leading-snug">
+                      {default_branch_commit_count}
+                    </Text>
+                    <Text size={1} className="leading-snug text-foreground-5">
+                      {t('views:repos.commits', 'Commits')}
+                    </Text>
+                  </div>
+                  <div className="flex flex-col justify-center rounded-md bg-background-4/80 py-1 px-3">
+                    <Text size={3} className="leading-snug">
+                      {branch_count}
+                    </Text>
+                    <Text size={1} className="leading-snug text-foreground-5">
+                      {t('views:repos.branches', 'Branches')}
+                    </Text>
+                  </div>
+                  <div className="flex flex-col justify-center rounded-md bg-background-4/80 py-1 px-3">
+                    <Text size={3} className="leading-snug">
+                      {tag_count}
+                    </Text>
+                    <Text size={1} className="leading-snug text-foreground-5">
+                      {t('views:repos.tags', 'Tags')}
+                    </Text>
+                  </div>
+                  <div className="flex flex-col justify-center rounded-md bg-background-4/80 py-1 px-3">
+                    <Text size={3} className="leading-snug">
+                      {pull_req_summary?.open_count || 0}
+                    </Text>
+                    <Text size={1} className="leading-snug  text-foreground-5">
+                      Open PRs
+                    </Text>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-3">
+              <div className="flex gap-2.5 justify-between items-center">
+                <Text size={3} className="tracking-snug text-foreground-1 col-span-2">
+                  Code vulnerabilities
+                </Text>
+                <Badge size={'sm'} theme="emphasis">
+                  3
+                </Badge>
+              </div>
+              <div className="flex flex-col gap-5">
+                <div className="relative flex w-full py-5 px-5 bg-background-4 flex-col items-center gap-2.5 text-center border rounded-md">
+                  <div className="flex flex-col items-center gap-1">
+                    <Text className="leading-snug text-foreground-1" size={2} weight="normal">
+                      Package.json
+                    </Text>
+                    <Spacer size={2} />
+                    <Text className="leading-snug text-destructive" size={1}>
+                      1 critical issue
+                    </Text>
+                    <Spacer size={3} />
+                    <Button
+                      className="bg-background-7 text-12 font-medium"
+                      borderRadius="full"
+                      size="default"
+                      padding="sm"
+                      variant="gradient-border"
+                      gradientType="ai-button"
+                    >
+                      <Icon className="mr-1.5" name="sparks" size={12} />
+                      Preview fix
+                    </Button>
+                  </div>
+                </div>
+                <div className="relative flex w-full py-5 px-5 bg-background-4 flex-col items-center gap-2.5 text-center border rounded-md">
+                  <div className="flex flex-col items-center gap-1">
+                    <Text className="leading-snug text-foreground-1" size={2} weight="normal">
+                      App.tsx
+                    </Text>
+                    <Spacer size={2} />
+                    <Text className="leading-snug text-warning" size={1}>
+                      2 medium issues
+                    </Text>
+                    <Spacer size={3} />
+                    <Button
+                      className="bg-background-7 text-12 font-medium"
+                      borderRadius="full"
+                      size="default"
+                      padding="sm"
+                      variant="gradient-border"
+                      gradientType="ai-button"
+                    >
+                      <Icon className="mr-1.5" name="sparks" size={12} />
+                      Preview fixes
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-3">
+              <div className="flex gap-2.5 justify-between items-center">
+                <Text size={3} className="tracking-snug text-foreground-1 col-span-2">
+                  Review PRs with AI
+                </Text>
+                <Badge size={'sm'} theme="emphasis">
+                  1
+                </Badge>
+              </div>
+              <div className="relative flex w-full py-5 px-5 bg-background-4 flex-col items-center gap-2.5 text-center border rounded-md">
+                <div className="flex flex-col items-center gap-1">
+                  <Text className="leading-snug text-foreground-1" size={2} weight="normal">
+                    "Correct all CSS root vars for light mode"
+                  </Text>
+                  <Spacer size={2} />
+                  <Text className="leading-snug text-foreground-4" size={1}>
+                    Raised by ARamina
+                  </Text>
+                  <Spacer size={3} />
+                  <Button
+                    className="bg-background-7 text-12 font-medium"
+                    borderRadius="full"
+                    size="default"
+                    padding="sm"
+                    variant="gradient-border"
+                    gradientType="ai-button"
+                  >
+                    <Icon className="mr-1.5" name="sparks" size={12} />
+                    Start AI review
+                  </Button>
+                </div>
+              </div>
+            </div>
           </SandboxLayout.Content>
         </SandboxLayout.Column>
       </SandboxLayout.Columns>
